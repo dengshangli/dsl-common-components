3da91e1f509dddb4ec948a7c14b5a8033e6382e9
dengshangli <dengshangli@bdfint.com>
2022-07-27 10:19:39 +0800

    测试

--- .umirc.ts
+++ .umirc.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'dumi';
+const repo = 'react-uni-comps';
 
 export default defineConfig({
   title: 'dsl-components',
@@ -6,8 +7,9 @@ export default defineConfig({
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
+  mode: 'site',
   history: { type: 'hash' },
-  base: '/<REPO>/',
-  publicPath: '/<REPO>/',
+  base: `/${repo}/`,
+  publicPath: `/${repo}/`,
   // more config: https://d.umijs.org/config
 });
